a9e90d9402f4d5bc8cbb79b671292f40b41b1875
didrudcks <rudcksdid@naver.com>
2020-10-16 12:50:54 +0900

    Jake

--- src/java_board/Test.java
+++ src/java_board/Test.java
@@ -1,10 +1,83 @@
 package java_board;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner sc = new Scanner(System.in);
+		boolean loop = true;
+		Add a = new Add();
+		
+		while(loop)
+		{
+			System.out.print("명령어 입력 : ");
+			
+			String s = sc.next(); // 입력
+			
+			
+			if(s.equals("add"))
+			{
+				a.add();
+			}
+			
+			else if(s.equals("list"))
+			{
+				System.out.println("제목 : " + a.title);
+				System.out.println("내용 : " + a.body);
+			}
+			
+			else if(s.equals("exit"))
+			{
+				Exit e = new Exit();
+				loop = e.exit(s, loop); // 종료
+			}
+			
+		}
+		
+		
+		
+		
 	}
 
+}
+
+class Exit {
+	boolean exit (String s, boolean loop)
+	{
+		if(s.equals("exit"))
+		{
+			System.out.println("종료");
+			loop = false;
+		}
+		return loop;
+	}
+}
+
+class Add {
+	String title;
+	String body;
+	Scanner sc2 = new Scanner(System.in);
+	
+	void add ()
+	{
+		System.out.print("게시물 제목을 입력해주세요 : ");
+		title = sc2.next();
+		
+		System.out.print("게시물 내용을 입력해주세요 : ");
+		body = sc2.next();
+		
+		System.out.println("게시물이 등록 되었습니다.");
+	}
+}
+
+class List {
+	ArrayList list = new ArrayList();
+	
+	void show_list()
+	{
+		
+	}
 }
