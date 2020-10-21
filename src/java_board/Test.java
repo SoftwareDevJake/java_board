d444f251114e32eed6628e49ca49c16e91a1c935
didrudcks <rudcksdid@naver.com>
2020-10-21 11:09:09 +0900

    게시물 목록화: l
    exit

--- src/java_board/Test.java
+++ src/java_board/Test.java
@@ -4,69 +4,107 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Test {
-
 	public static void main(String[] args) {
-
 		Scanner sc = new Scanner(System.in);
-		ArrayList titles = new ArrayList();
-		ArrayList bodies = new ArrayList();
-		boolean loop = true;
-		Add a = new Add();
+		ArrayList<Integer> ids = new ArrayList<>();
+		ArrayList<String> titles = new ArrayList<>();
+		ArrayList<String> bodies = new ArrayList<>();
+		int size = 0;
+		ArrayList<Article> articles = new ArrayList<>();
 
-		while (loop) {
+		int no = 1;
+
+		while (true) {
+			
 			System.out.print("명령어 입력 : ");
-
-			String s = sc.next(); // 입력
-
-			if (s.equals("add")) {
-				a.add(titles, bodies);
+			String cmd = sc.next();
+			if (cmd.equals("exit")) {
+				System.out.println("종료");
+				break;
 			}
+	
+			if (cmd.equals("add")) {
 
-			else if (s.equals("list")) {
-				for (int i = 0; i < titles.size(); i++) {
+				Article a = new Article();
+
+				a.id = no;
+
+				ids.add(no);
+				no++;
+				System.out.println("게시물 제목을 입력해주세요 :");
+				String title = sc.next();
+				a.title = title;
+				titles.add(title);
+				System.out.println("게시물 내용을 입력해주세요 :");
+				String body = sc.next();
+				a.body = body;
+				bodies.add(body);
+
+				articles.add(a);
+				System.out.println("게시물이 등록되었습니다.");
+				size++;
+
+			}
+			if (cmd.equals("list")) {
+				for (int i = 0; i < ids.size(); i++) {
+					System.out.println("번호 : " + ids.get(i));
 					System.out.println("제목 : " + titles.get(i));
 					System.out.println("내용 : " + bodies.get(i));
+					System.out.println("===================");
 				}
 			}
+			
+			if (cmd.equals("delete")) {
+				System.out.println("수정할 게시물 선택 : ");
+				int targetId = sc.nextInt();
 
-			else if (s.equals("exit")) {
-				Exit e = new Exit();
-				loop = e.exit(s, loop); // 종료
+				for (int i = 0; i < ids.size(); i++) {
+					int id = ids.get(i);
+
+					if (id == targetId) {
+						System.out.println("게시물 제목을 입력해주세요 :");
+						String newTitle = sc.next();
+						
+
+						System.out.println("게시물 내용을 입력해주세요 :");
+						String newBody = sc.next();
+						
+
+						titles.set(i, newTitle);
+						bodies.set(i, newBody);
+						break;
+					}
+				}
 			}
+			if (cmd.equals("delete")) {
+				System.out.print("몇번 게시물을 지우시겠습니까 :");
+				int targetId = sc.nextInt();
+				int existFlag = 2;// 1. 있음 2. 없음
 
+				for (int i = 0; i < ids.size(); i++) {
+					int id = ids.get(i);
+					if (id == targetId) {
+						existFlag = 1;
+						ids.remove(i);
+						titles.remove(i);
+						bodies.remove(i);
+						break;
+					} 
+				}
+
+				if(existFlag == 2) {
+					System.out.println("게시물이 존재하지 않습니다.");					
+				} else {
+					System.out.println(targetId + "번 게시물이 삭제되었습니다.");					
+				}
+
+			}
 		}
-
 	}
+}	
 
-}
-
-class Exit {
-	boolean exit(String s, boolean loop) {
-		if (s.equals("exit")) {
-			System.out.println("종료");
-			loop = false;
-		}
-		return loop;
-	}
-}
-
-class Add {
-	Scanner sc2 = new Scanner(System.in);
-
-	void add(ArrayList titles, ArrayList bodies) {
-		System.out.print("게시물 제목을 입력해주세요 : ");
-		titles.add(sc2.next());
-
-		System.out.print("게시물 내용을 입력해주세요 : ");
-		bodies.add(sc2.next());
-
-		System.out.println("게시물이 등록 되었습니다.");
-	}
-}
-
-class List {
-
-	void show_list() {
-
-	}
+class Article {
+	int id;
+	String title;
+	String body;
 }
