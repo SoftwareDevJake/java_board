932b431521423b8bd97944bf8e341263368fe5a6
didrudcks <rudcksdid@naver.com>
2020-11-11 11:02:28 +0900

    jake

--- src/java_board/java_board.java
+++ src/java_board/java_board.java
@@ -10,6 +10,9 @@ public class java_board {
 
 		ArrayList<String> titles = new ArrayList<>();
 		ArrayList<String> bodies = new ArrayList<>();
+		
+		//int count_title = -1;
+		//int count_body = -1;
 
 		while (true) {
 			System.out.print("명령어 입력 : ");
@@ -22,8 +25,11 @@ public class java_board {
 
 				System.out.println("게시물 제목을 입력해주세요 :");
 				titles.add(sc.next());
+				//count_title++;
+				
 				System.out.println("게시물 내용을 입력해주세요 :");
 				bodies.add(sc.next());
+				//count_body++;
 				System.out.println("게시물이 등록되었습니다.");
 
 			}
@@ -33,6 +39,34 @@ public class java_board {
 					System.out.println("내용 : " + bodies.get(i));
 					System.out.println("======================");
 				}
+			}
+			if (cmd.equals("update"))
+			{
+				System.out.println("업데이트 할 게시물을 선택해 주세요.");
+				String update;
+				update = sc.next();
+				
+				for(int i = 0; i < titles.size(); i++)
+				{
+					if(titles.get(i).equals(update))
+					{
+						titles.remove(i);
+						bodies.remove(i);
+						
+						System.out.println("업데이트 제목을 입력해 주세요");
+						titles.add(sc.next());
+						
+						System.out.println("업데이트 내용을 입력해 주세요.");
+						bodies.add(sc.next());
+						
+						System.out.println("업데이트가 완료 되었습니다.");
+						break;
+					}
+					
+				}
+				
+				System.out.println();
+				
 			}
 		}
 
